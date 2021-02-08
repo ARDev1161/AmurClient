a2bb71bac32d8126917d03f467f1862a6bf494dd
ARDev1161 <radtarasov@gmail.com>
2021-02-08 09:54:02 +0300

    Update hc595.h

--- modules/registers/hc595/hc595.h
+++ modules/registers/hc595/hc595.h
@@ -1,10 +1,10 @@
 #ifndef HC595_H
 #define HC595_H
 
+#include <iostream>
+
 #if __has_include(<pigpio.h>)
     #include <pigpio.h>
-#else
-    #include <iostream> // for debugging
 #endif
 
 #include "../registerssettings.h"
