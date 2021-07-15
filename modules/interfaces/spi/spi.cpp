30e87593902ea835b65e2ff9f6ef0a9f5f20149c
Radomir <radtarasov@gmail.com>
2021-07-15 23:09:48 +0300

    Redundant blank lines are deleted.

--- modules/interfaces/spi/spi.cpp
+++ modules/interfaces/spi/spi.cpp
@@ -5,7 +5,6 @@
 */
 SPI::SPI()
 {
-
 }
 
 /*!
@@ -13,12 +12,10 @@ SPI::SPI()
   \return Возвращает 0 если удалось открыть порт
 */
 int SPI::init(){
-
 #if __has_include(<pigpio.h>)
 
 #else
     std::cout << "SPI initializing" << std::endl ;
     return 0;
 #endif
-
 }
