8f6c8ef633c103cff6d5d595b621a4a9aa6c45c2
ARDev1161 <radtarasov@gmail.com>
2021-10-11 00:35:36 +0300

    Update pwmcontroller.h

--- modules/pwm/pwmcontroller.h
+++ modules/pwm/pwmcontroller.h
@@ -17,9 +17,12 @@
 */
 class PWMController
 {
-    int pwmRange = 255;
-    int freq = 20; // PWM frequency in kilohertz
+    int pwmRange = 255; //Max input value, min value is 0
+    int freqHardware = 20000; // PWM frequency in hertz
 
+    inline int translateDuty(int duty, int newMax){
+        return ((duty * newMax) / pwmRange);
+    }
 public:
     PWMController();
 
@@ -30,6 +33,12 @@ public:
     int softPWMCreate(int pin);
     int softPWMChange(int pin, int dutyCycle);
     int softPWMStop(int pin);
+
+    int getPwmRange() const;
+    void setPwmRange(int newPwmRange);
+
+    int getFreqHardware() const;
+    void setFreqHardware(int newFreqHardware);
 };
 
 #endif // PWMCONTROLLER_H
