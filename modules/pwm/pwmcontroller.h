d25c41f23977980f8da3ad16abe10461eefac0a2
ARDev1161 <radtarasov@gmail.com>
2021-10-14 00:04:34 +0300

    Update pwmcontroller.h

--- modules/pwm/pwmcontroller.h
+++ modules/pwm/pwmcontroller.h
@@ -20,26 +20,26 @@ class PWMController
     unsigned int pwmRange = 255; //Max input value, min value is 0
     unsigned int freqHardware = 200; // PWM frequency in hertz
 
-    inline unsigned int translateDuty(unsigned int duty, unsigned int newMax){
+    inline unsigned int translateDuty(unsigned int duty){
         return ((duty / pwmRange) * (375000000 / freqHardware)); // For non BCM2711 use 250M
     }
     
 public:
     PWMController();
 
-    int hardPWMCreate(int pin);
-    int hardPWMChange(int pin, int dutyCycle);
-    int hardPWMStop(int pin);
+    int hardPWMCreate(unsigned int pin);
+    int hardPWMChange(unsigned int pin, unsigned int dutyCycle);
+    int hardPWMStop(unsigned int pin);
 
-    int softPWMCreate(int pin);
-    int softPWMChange(int pin, int dutyCycle);
-    int softPWMStop(int pin);
+    int softPWMCreate(unsigned int pin);
+    int softPWMChange(unsigned int pin, unsigned int dutyCycle);
+    int softPWMStop(unsigned int pin);
 
     int getPwmRange() const;
-    void setPwmRange(int newPwmRange);
+    void setPwmRange(unsigned int newPwmRange);
 
     int getFreqHardware() const;
-    void setFreqHardware(int newFreqHardware);
+    void setFreqHardware(unsigned int newFreqHardware);
 };
 
 #endif // PWMCONTROLLER_H
