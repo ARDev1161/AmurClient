ebd9838dd29619c822884b69f452971f5637067a
ARDev1161 <radtarasov@gmail.com>
2023-03-20 13:48:56 +0300

    Update grpc.sh

--- .github/workflows/scripts/grpc.sh
+++ .github/workflows/scripts/grpc.sh
@@ -6,25 +6,10 @@ export PATH="$MY_INSTALL_DIR/bin:$PATH"
 
 sudo apt install -y build-essential autoconf automake libtool pkg-config curl make g++ unzip
 
-echo "Installing Protocol Buffers"
-git clone https://github.com/protocolbuffers/protobuf.git
-cd protobuf
-git submodule update --init --recursive
-./autogen.sh
-./configure
-make
-make check
-sudo make install
-sudo ldconfig # refresh shared library cache.
-
-pkg-config --cflags protobuf         # print compiler flags
-pkg-config --libs protobuf           # print linker flags
-pkg-config --cflags --libs protobuf  # print both
-
 echo "Installing gRPC"
-git clone --recurse-submodules -b v1.38.0 https://github.com/grpc/grpc
-cd grpc
+git clone --recurse-submodules -b v1.52.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
 
+cd grpc
 mkdir -p cmake/build
 pushd cmake/build
 cmake -DgRPC_INSTALL=ON -DgRPC_BUILD_TESTS=OFF \
@@ -34,12 +19,31 @@ make -j4
 make install
 popd
 
-echo "Installing Abseil"
-mkdir -p third_party/abseil-cpp/cmake/build
-pushd third_party/abseil-cpp/cmake/build
-cmake -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
-      -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE \
-      ../..
-make -j4
-make install
-popd
+# echo "Installing Protocol Buffers"
+# git clone https://github.com/protocolbuffers/protobuf.git
+# cd protobuf
+# git submodule update --init --recursive
+# ./autogen.sh
+# ./configure
+# make
+# make check
+# sudo make install
+# sudo ldconfig # refresh shared library cache.
+
+# pkg-config --cflags protobuf         # print compiler flags
+# pkg-config --libs protobuf           # print linker flags
+# pkg-config --cflags --libs protobuf  # print both
+
+
+# git clone --recurse-submodules -b v1.38.0 https://github.com/grpc/grpc
+
+
+# echo "Installing Abseil"
+# mkdir -p third_party/abseil-cpp/cmake/build
+# pushd third_party/abseil-cpp/cmake/build
+# cmake -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
+#       -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE \
+#       ../..
+# make -j4
+# make install
+# popd
