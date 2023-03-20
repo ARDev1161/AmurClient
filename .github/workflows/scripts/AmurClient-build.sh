dd9d408b8382c8b0a90bfe12e3a2f481dcca0148
ARDev1161 <radtarasov@gmail.com>
2023-03-20 17:10:21 +0300

    Update AmurClient-build.sh

--- .github/workflows/scripts/AmurClient-build.sh
+++ .github/workflows/scripts/AmurClient-build.sh
@@ -4,6 +4,6 @@ git clone https://github.com/ARDev1161/AmurClient.git
 cd AmurClient
 mkdir bin && cd bin
 
-cmake -DCMAKE_PREFIX_PATH=/opt/hostedtoolcache/grpc/1.48.2/x64 -DCMAKE_BUILD_TYPE=Release -DGRPC_FETCHCONTENT=OFF ../
+cmake -DCMAKE_LIBRARY_PATH=/opt/hostedtoolcache/grpc/1.48.2/x64/lib -DCMAKE_PREFIX_PATH=/opt/hostedtoolcache/grpc/1.48.2/x64 -DCMAKE_BUILD_TYPE=Release -DGRPC_FETCHCONTENT=OFF ../
 
 cmake --build . -- -j4
