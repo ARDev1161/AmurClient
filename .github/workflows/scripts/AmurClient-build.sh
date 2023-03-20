ee1f8e0270dce1a4a8e05617b78e3fd0a3bc7353
ARDev1161 <radtarasov@gmail.com>
2023-03-20 16:43:10 +0300

    Update AmurClient-build.sh

--- .github/workflows/scripts/AmurClient-build.sh
+++ .github/workflows/scripts/AmurClient-build.sh
@@ -4,7 +4,6 @@ git clone https://github.com/ARDev1161/AmurClient.git
 cd AmurClient
 mkdir bin && cd bin
 
-# If gRPC is globally installed
-cmake -DCMAKE_PREFIX_PATH=${GRPC_ROOT}/lib/cmake/grpc -DCMAKE_BUILD_TYPE=Release -DGRPC_FETCHCONTENT=OFF ../
+cmake -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} -DCMAKE_BUILD_TYPE=Release -DGRPC_FETCHCONTENT=OFF ../
 
 cmake --build . -- -j4
