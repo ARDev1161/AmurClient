f42effb9823ed368957d8c5c514d355feda0f95b
ARDev1161 <radtarasov@gmail.com>
2023-04-04 02:26:17 +0300

    Update grpc.sh

--- .github/workflows/scripts/grpc.sh
+++ .github/workflows/scripts/grpc.sh
@@ -1,35 +1,24 @@
 #!/bin/bash
 
-apt-get install -y \
-protobuf-compiler \
-protobuf-compiler-grpc \
-protobuf-c-compiler \
-libprotobuf-dev \
-libprotobuf-c-dev \
-libgrpc++-dev \
-libgrpc++1 \
-libgrpc6
-
-
-# export MY_INSTALL_DIR=$HOME/.local
-# mkdir -p $MY_INSTALL_DIR
-# export PATH="$MY_INSTALL_DIR/bin:$PATH"
+export MY_INSTALL_DIR=$HOME/.local
+mkdir -p $MY_INSTALL_DIR
+export PATH="$MY_INSTALL_DIR/bin:$PATH"
 
 # sudo apt-get remove -y protobuf-compiler
-# sudo apt install -y build-essential autoconf automake libtool pkg-config curl make g++ unzip
+sudo apt install -y build-essential autoconf automake libtool pkg-config curl make g++ unzip
 
 # echo "Installing gRPC"
-# git clone --recurse-submodules -b v1.52.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
+git clone --recurse-submodules -b v1.52.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
 
-# cd grpc
-# mkdir -p cmake/build
-# pushd cmake/build
-# cmake -DgRPC_INSTALL=ON -DgRPC_BUILD_TESTS=OFF \
-#       -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
-#       ../..
-# make -j4
-# sudo make install
-# popd
+cd grpc
+mkdir -p cmake/build
+pushd cmake/build
+cmake -DgRPC_INSTALL=ON -DgRPC_BUILD_TESTS=OFF \
+      -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
+      ../..
+make -j4
+make install
+popd
 
 # echo "Installing Protocol Buffers"
 # git clone https://github.com/protocolbuffers/protobuf.git
